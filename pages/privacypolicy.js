b20fa3313858af08b7e080dd9cd2cd753eaa4fb8
Tech-Kairaa <tech@kairaa.in>
2024-01-09 16:31:10 +0530

    Update privacy policy
    git add.

--- pages/privacypolicy.js
+++ pages/privacypolicy.js
@@ -2,7 +2,7 @@ import React from "react";
 import PageBanner from "../src/components/PageBanner";
 import Layout from "../src/layout/Layout";
 
-const privacypolicy = () => {
+function privacypolicy() {
   return (
     <div>
       <Layout>
@@ -11,9 +11,9 @@ const privacypolicy = () => {
           <div className="container">
             <div className="row">
               <div className="col-lg-8 " style={{ width: "100%" }}>
-                <div className="Privacypolicy-details-wrap">   
+                <div className="Privacypolicy-details-wrap">
                   <div className="image mb-25 wow fadeInUp delay-0-2s">
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Privacy Policy.</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Privacy Policy.</h2>
                     <p>
                       The way that kairaaacademy gathers, utilizes, preserves,
                       and disseminates data obtained from visitors to the
@@ -21,7 +21,7 @@ const privacypolicy = () => {
                       policy. This privacy statement covers the Site as well as
                       all of kairaaacademy's goods and services.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}> Personal identification information</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}> Personal identification information</h2>
                     <p>
                       Users may provide us with personal identification
                       information in a number of ways, such as when they visit
@@ -36,7 +36,7 @@ const privacypolicy = () => {
                       information, albeit doing so can make it impossible for
                       them to participate in some site related activities.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Non-personal identification information</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Non-personal identification information</h2>
                     <p>
                       Every time a User interacts with our site, we might get
                       non personal identification information about them.
@@ -46,7 +46,7 @@ const privacypolicy = () => {
                       our website, including the operating system and ISP they
                       were using, among other things.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Web browser cookies</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Web browser cookies</h2>
                     <p>
                       "Cookies" may be used by our site to improve user
                       experience. Cookies are files that a user's web browser
@@ -56,7 +56,7 @@ const privacypolicy = () => {
                       cookies are being sent. If they do, be aware that the Site
                       might not work properly in some areas.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>How we use collected information</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>How we use collected information</h2>
                     <p>
                       The following uses of user personal information that
                       kairaaacademy may gather and employ To enhance client
@@ -66,7 +66,7 @@ const privacypolicy = () => {
                       might reply to their queries, questions, and other
                       requests using the email address.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}> How we protect your information</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}> How we protect your information</h2>
                     <p>
                       In order to prevent unauthorized access, alteration,
                       disclosure, or destruction of your personal information,
@@ -74,7 +74,7 @@ const privacypolicy = () => {
                       stored on our site, we implement suitable data collecting,
                       storage, and processing policies and security measures.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Sharing your personal information</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Sharing your personal information</h2>
                     <p>
                       We don't give away, trade, or rent to third parties User
                       personal identification information. We may provide our
@@ -83,7 +83,7 @@ const privacypolicy = () => {
                       connected to any personally identifiable information about
                       visitors and users for the uses mentioned above.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Changes to this privacy policy</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Changes to this privacy policy</h2>
                     <p>
                       This privacy statement may be updated at any time by
                       kairaaacademy. We'll send you an email as soon as we do.
@@ -93,7 +93,7 @@ const privacypolicy = () => {
                       to regularly examine this privacy statement and become
                       informed of any updates.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Your acceptance of these terms</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Your acceptance of these terms</h2>
                     <p>
                       You agree to the terms of service and this policy by using
                       this site. Please do not use our site if you disagree with
@@ -101,7 +101,7 @@ const privacypolicy = () => {
                       you will be deemed to have accepted those changes by
                       continuing to use the Site.
                     </p>
-                    <h2 style={{fontSize:"35px",marginTop:"20px" ,fontFamily:"'Font Awesome 5 Free'"}}>Contacting us</h2>
+                    <h2 style={{ fontSize: "35px", marginTop: "20px", fontFamily: "'Font Awesome 5 Free'" }}>Contacting us</h2>
                     <p>
                       Please contact us at kairaaacademy www.kairaaacademy.com
                       10, Easwari Nagar, Gowriwakkam, Sembakkam, Chennai, Tamil
@@ -119,6 +119,6 @@ const privacypolicy = () => {
       </Layout>
     </div>
   );
-};
+}
 
 export default privacypolicy;
